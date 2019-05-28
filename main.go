4316a4b4501fd5d2d9acce3e71556555f0a5c825
Michał Kubik <bopke2@gmail.com>
2019-05-28 23:12:49 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/bopke/korwin"
+	"github.com/bwmarrin/discordgo"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	session, err := discordgo.New("Bot " + os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	session.AddHandler(OnMessageCreate)
+	err = session.Open()
+	if err != nil {
+		panic(err)
+	}
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+	err = session.Close()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func OnMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	for _, mention := range m.Mentions {
+		if mention.ID == s.State.User.ID {
+			_, _ = s.ChannelMessageSend(m.ChannelID, korwin.GenerateStatement())
+			return
+		}
+	}
+}
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Korwin Discord Bot
+
+Niesamowicie zaawansowany bot na discorda, generujący wypowiedzi w stylu krula.
+
+Zależności
+--
+
+```
+go get github.com/bopke/korwin
+go get github.com/bwmarrin/discordgo
+```
+
